a831aefb6d4a0df8163e79119b9ddb1d12a10b63
AndreyTishchenko <Tishchenko.Andrew@yandex.ru>
2024-10-23 17:55:50 +0400

    fix: fix-slider-functionality

--- coffee-house/scripts/slider.js
+++ coffee-house/scripts/slider.js
@@ -7,11 +7,31 @@ new Swiper('.swiper', {
   autoplay: {
     delay: 2500
   },
+pagination: {
+    el: ".swiper_pagination",
+    renderBullet: function (index, className) {
+        return '<span class="' + className + '">' + '<div class="progress"></div>' + "</span>";
+    },
+  },
   navigation: {
     nextEl: '.next_page',
     prevEl: '.previous_page',
   },
   allowTouchMove: true,
+  on: {
+    autoplayTimeLeft(s, time, progress) {
+        const progressBar = document.querySelector('.swiper-pagination-bullet-active .progress');
+        progressBar.style.width = `${100 - progress * 100}%`;
+        console.log(progressBar.style.width)
+    },
+    slideChange: function () {
+      const progressBars = document.querySelectorAll('.swiper_pagination .progress');
+      console.log(progressBars)
+      progressBars.forEach(progressBar => {
+        progressBar.style.width = '0%'
+      });
+    }
+  },
   breakpoints: {
     768: {
         allowTouchMove: false,
